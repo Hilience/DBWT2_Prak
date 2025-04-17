9e873d3d1afb839888d6e2f301e81e6ce8b302b7
tolimb <152211662+tolimb@users.noreply.github.com>
2025-04-17 14:52:37 +0200

    Added home view.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <nav id="nav"></nav>
+
+    <footer id="cookie_check">
+
+    </footer>
+</body>

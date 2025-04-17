251f14c8d87a7fd0e3b901e02a4bb41a0ad700c8
Hilience <ali.tayba@alumni.fh-aachen.de>
2025-04-17 15:02:36 +0200

    Bla

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <script src="cookieFenster.js"></script>
+    <script src="/resources/js/cookieFenster.js"></script>
 </head>
 <body>
     <nav id="nav"></nav>
